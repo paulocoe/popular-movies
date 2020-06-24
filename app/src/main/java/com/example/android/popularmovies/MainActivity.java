63f1df790717bde65c9bd9c59d113b94ccca302f
paulocoe <paulo_coe@hotmail.com>
2020-06-24 21:38:31 +0100

    Adds empty adapter and card view for movie poster

--- app/src/main/java/com/example/android/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/android/popularmovies/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.android.popularmovies;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -10,20 +12,28 @@ import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView mRecyclerView;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private RecyclerView.Adapter mAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mRecyclerView = findViewById(R.id.rv_main_grid);
+        mRecyclerView.setHasFixedSize(true);
+
+        mLayoutManager = new GridLayoutManager(this, 3);
+
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mAdapter = new MovieAdapter();
+        mRecyclerView.setAdapter(mAdapter);
     }
 
     private void loadMovies(){
         //TODO: Replace the hardcoded sorting param with the value stored in the options
         new FetchMoviesTask().execute("popular", "1");
     }
-
-
-
-
 
     public class FetchMoviesTask extends AsyncTask<String, Void, Movie[]>{
 
